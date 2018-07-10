7d8f02a653eb34ed2ecc0c549545023d1dce564a
MiroslavaTatsenko <tmira@ukr.net>
2018-07-10 15:32:45 +0300

    draw chart

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
-from flask import Flask, render_template, request, url_for, json
+from flask import Flask, render_template, request, url_for, jsonify
 from functools import wraps
 
 from parser.parser import ParserSalaries
 from dataBase.dataBase import DataBase
+
+data = ParserSalaries('/home/mira/projects/soapBox/parser/data/')
+db = DataBase()
 
 @app.route('/')
 def home():
@@ -13,36 +16,27 @@ def home():
 @app.route('/viewData')
 
 def viewDataStart():
-	data = ParserSalaries('/home/mira/projects/soapBox/parser/data/')
 	return render_template('viewDataStart.html', listNameFiles = data.listNameFiles)
 
 
 @app.route('/viewData/<string:nameFile>')
 
 def viewData(nameFile = ''):
-	db = DataBase()
 	return render_template('viewData.html', db = db.selectData(nameFile))
 
 
 @app.route('/viewSalary', methods=['GET'])
 
 def viewSalary():
-	data = ParserSalaries('/home/mira/projects/soapBox/parser/data/')
 	return render_template('viewSalary.html', listNameFiles = data.listNameFiles)
 
 
 @app.route('/viewSalary', methods=['POST'])
 
 def viewChart():
-	chartOption = request.form['chartOption']
 	nameFile = request.form['nameFile']
-	data = ParserSalaries('/home/mira/projects/soapBox/parser/data/')
-	db = DataBase()
-	listAverageSalary = []
-	listComparator = []
-	for elem in db.selectAverageSalary(nameFile, chartOption):
-		listComparator.append(elem[0])
-		listAverageSalary.append(elem[1])
-	#return render_template('viewChart.html', listNameFiles = data.listNameFiles, listAverageSalary = listAverageSalary)
-	return json.dumps({'listAverageSalary': listAverageSalary, 
-						'listComparator': listComparator})
+	chartOption = request.form['chartOption']
+	listAverageSalary = [x[1] for x in db.selectAverageSalary(nameFile, chartOption)]
+	listComparator = [x[0] for x in db.selectAverageSalary(nameFile, chartOption)] #div data on two lists for comfort
+	return render_template('viewSalary.html', listNameFiles = data.listNameFiles, 
+		listAverageSalary = listAverageSalary, listComparator = listComparator, year = nameFile)
